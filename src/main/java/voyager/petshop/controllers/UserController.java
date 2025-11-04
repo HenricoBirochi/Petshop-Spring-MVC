f0d73a8fe29f1f30770fe3e162226c4faa915609
Henrico Birochi <henrico.birochi@gmail.com>
2025-11-04 17:44:18 -0300

    feat: Implement user registration and login functionality, add user verification utility, and create associated HTML templates

--- src/main/java/voyager/petshop/controllers/UserController.java
+++ src/main/java/voyager/petshop/controllers/UserController.java
@@ -1,16 +1,37 @@
 package voyager.petshop.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import voyager.petshop.models.User;
+import voyager.petshop.repositories.UserRepository;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-    @GetMapping("/login")
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/sing-up")
+    public String signUpPage() {
+        return "user/sing_up";
+    }
+
+    @PostMapping("/sing-up")
+    public String signUpPageForm(User user) {
+        if (user != null)
+            userRepository.save(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/sing-in")
     public String getMethodName() {
-        return "/user/login";
+        return "user/sing_in";
     }
 
 }
