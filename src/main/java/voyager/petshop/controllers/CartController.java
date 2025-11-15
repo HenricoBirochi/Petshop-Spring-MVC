96a076195e839c09b8d974f6ebb5a61dd974bec6
Henrico Birochi <henrico.birochi@gmail.com>
2025-11-15 12:40:44 -0300

    feat: Implement cart empty functionality with add-item endpoint

--- src/main/java/voyager/petshop/controllers/CartController.java
+++ src/main/java/voyager/petshop/controllers/CartController.java
@@ -2,11 +2,27 @@ package voyager.petshop.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 @Controller
 @RequestMapping("/cart")
 public class CartController {
 
-    
+
+    @GetMapping("")
+    public ModelAndView getMethodName() {
+        var mv = new ModelAndView("");
+        return mv;
+    }
+
+    @PostMapping("/add-item")
+    public ModelAndView postMethodName() {
+        var mv = new ModelAndView("");
+        return mv;
+    }
 
 }
